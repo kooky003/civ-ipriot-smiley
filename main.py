942451884dde0d79e74a43cd5a1fd3ea81d1e870
20125134 <20125134@tafe.wa.edu.au>
2024-05-09 13:58:28 +0800

    Addressed requirement of the portfolio

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ That is, delete the sense_hat.py file that is included in this bundle."""
 import time
 
 from happy import Happy
+from sad import Sad
 
 if __name__ == '__main__':
     # This is only needed if you have not deleted sense_hat.py
@@ -14,7 +15,7 @@ if __name__ == '__main__':
 
     # Create a happy smiley, which is a subclass of Smiley
     smiley = Happy()
-
+    # smiley = Sad()
     # This is a form of #polymorphism, as the Happy smiley class
     # does not have a method called .show(). This means that
     # the method .show() of the base class {Smiley} will be
@@ -29,4 +30,5 @@ if __name__ == '__main__':
     # The method blink is implemented by the Happy class, but
     # is defined as an interface (i.e., an abstract base class
     # with an abstract method).
+
     smiley.blink()
